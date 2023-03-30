9a9b3ed28c85fb030137e0f27ac32d35fd52753c
Kunal31 <Kunalpandav31@gmail.com>
2023-03-30 18:47:00 +0530

    changed UI for data entry added existing entry filter based on search keyword in Search textbox minor bug fixes

--- dashboard_app/urls.py
+++ dashboard_app/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, Insert_automation_data_view, table_automation_view, index, upload_excel, test
+from .views import home, Insert_automation_data_view, table_automation_view, index, upload_excel
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    #path('', home, name='Index'),
-    path('', index.as_view(), name='Index'),
+    path('', home, name='Home'),
+    path('dashboard/', index.as_view(), name='Dashboard'),
     path('list/createdata/', Insert_automation_data_view, name='Insert_automation_data'),
     path('list/', table_automation_view.as_view(), name='table_automation'),
     path('upload_excel/', upload_excel, name='upload_excel'),
